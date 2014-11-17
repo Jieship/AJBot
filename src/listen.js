9529fd791c3005dca8ead0e212da7898882b0e20
Jie Guan <jie.s.guan@gmail.com>
2014-11-17 17:21:02 -0500

    makes amazon.com redirect to ebay.com

--- src/listen.js
+++ src/listen.js
@@ -1,5 +1,7 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
-    if (changeInfo.status === 'complete') {
-        console.log("hello alice");
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url.indexOf("amazon.com") > -1) {
+        var updateParam = new Object();
+        updateParam.url = "http://ebay.com";
+        chrome.tabs.update(tabId, updateParam);
     }
 });
