d3744cbfbe91e9a5c16561e038baabbec9266d1f
Jie Guan <jie.s.guan@gmail.com>
2014-12-03 01:01:30 -0500

    optimized bookmark collusion.
    Now transfers 2^28 bytes in 6 seconds

--- collusions/catcher/src/catch.js
+++ collusions/catcher/src/catch.js
@@ -1,7 +1,10 @@
-chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
-    if (bookmark.title == "collusion payload") {
-        console.log("caught " + bookmark.index);
-    }
+chrome.bookmarks.onChanged.addListener(function(id, changeInfo) {
+    chrome.bookmarks.get(id, function(results) {
+        var bookmark = results[0];
+        if (bookmark.title === "collusion payload") {
+            console.log("caught chunk of size " + (bookmark.url.length - 1));
+        }
+    });
 });
 
 console.log("catcher ready!");
--- collusions/pitcher/src/pitch.js
+++ collusions/pitcher/src/pitch.js
@@ -1,18 +1,50 @@
-function pitch() {
-    console.log("pitcher started pitching");
-    for (var i=0; i < 10; i++) {
-        chrome.bookmarks.create({
-            'title': 'collusion payload',
-            'index': i
-        });
-    }
-    console.log("pitcher finished pitching");
+String.prototype.repeat = function(n) { 
+    return new Array(1 + (n || 0)).join(this);
 }
 
+var MAX_CHUNK_SIZE = 26843540;
+var PAYLOAD_SIZE = 268435400; /* 2^28 minus some overhead */
+var payload = "";
+while (payload.length < PAYLOAD_SIZE) {
+    payload += '' + Math.random();
+}
+payload = payload.substring(0, PAYLOAD_SIZE);
+
+var id; /* id of payload bookmark */
+
+/* sets up the id variable */
+function initialize() {
+    chrome.bookmarks.search("collusion payload", function (results) {
+        if (results.length === 0) {
+            chrome.bookmarks.create({
+                'title': 'collusion payload',
+                'url': 'http://place.holder.url'
+            }, function (newBookmark) {id = newBookmark.id});
+        } else {
+            id = results[0].id;
+        }
+    });
+}
+
+function pitch(message) {
+    if (message) {
+        console.log("pitching chunk of size " + message.substring(0, MAX_CHUNK_SIZE).length);
+        chrome.bookmarks.update(
+            id,
+            { 'url': ':' + message.substring(0, MAX_CHUNK_SIZE)}, 
+            function(result) { pitch(message.substring(MAX_CHUNK_SIZE)) }
+        );
+    } else {
+        console.log("transmission ended at " + Date.now());
+    }
+}
+
+initialize();
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.status == "complete") {
         if (tab.url.indexOf("baseball.com") > -1) {
-            pitch();
+            console.log("transmission began at " + Date.now());
+            pitch(payload);
         }
     }
 });
